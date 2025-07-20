ef052da67b9974f37ae73c0bb5a95c7b874e6abc
Biswa <kracksec@gmail.com>
2025-07-20 20:58:41 +0530

    updating site desc

--- client/app/layout.js
+++ client/app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "TERMI",
-  description: "termi, instant, anonymous chats for techies",
+  description: "instant, anonymous chats for techies",
 };
 
 export default function RootLayout({ children }) {
